ac5c95fc78ffd5826b0a1854b2b6cd670a6e8b2c
Roman Ožana <roman@ozana.cz>
2023-02-27 09:19:26 +0100

    Use tabs instead of spaces

--- src/cmd-action.ts
+++ src/cmd-action.ts
@@ -9,68 +9,68 @@ import style from './style.css?inline';
 
 @customElement('cmd-action')
 export class CmdAction extends LitElement {
-  
-  static override styles = unsafeCSS(style);
-  
-  /**
-   * The mode of the dialog (dark/light).
-   */
-  @property({type: String}) theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  
-  /**
-   * Action object
-   */
-  @property({type: Object}) action!: Action;
-  
-  /**
-   * Is the action selected
-   */
-  @property({type: Boolean}) selected = false;
-  
-  /**
-   * Scroll to show element
-   */
-  ensureInView() {
-    requestAnimationFrame(() => this.scrollIntoView({block: 'nearest'}));
-  }
-  
-  constructor() {
-    super();
-    this.addEventListener('click', this.click);
-  }
-  
-  /**
-   * Click event
-   */
-  override click() {
-    this.dispatchEvent(
-      new CustomEvent('actionSelected', {
-        detail: this.action,
-        bubbles: true,
-        composed: true,
-      })
-    );
-  }
-  
-  /**
-   * Updated
-   * @param changedProperties
-   */
-  override updated(changedProperties: Map<string, unknown>) {
-    if (changedProperties.has('selected')) {
-      if (this.selected) {
-        this.ensureInView();
-      }
-    }
-  }
-  
-  override render() {
-    const classes = {
-      selected: this.selected,
-      dark: this.theme === 'dark',
-    };
-    
-    return html`
+	
+	static override styles = unsafeCSS(style);
+	
+	/**
+	 * The mode of the dialog (dark/light).
+	 */
+	@property({type: String}) theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+	
+	/**
+	 * Action object
+	 */
+	@property({type: Object}) action!: Action;
+	
+	/**
+	 * Is the action selected
+	 */
+	@property({type: Boolean}) selected = false;
+	
+	/**
+	 * Scroll to show element
+	 */
+	ensureInView() {
+		requestAnimationFrame(() => this.scrollIntoView({block: 'nearest'}));
+	}
+	
+	constructor() {
+		super();
+		this.addEventListener('click', this.click);
+	}
+	
+	/**
+	 * Click event
+	 */
+	override click() {
+		this.dispatchEvent(
+			new CustomEvent('actionSelected', {
+				detail: this.action,
+				bubbles: true,
+				composed: true,
+			})
+		);
+	}
+	
+	/**
+	 * Updated
+	 * @param changedProperties
+	 */
+	override updated(changedProperties: Map<string, unknown>) {
+		if (changedProperties.has('selected')) {
+			if (this.selected) {
+				this.ensureInView();
+			}
+		}
+	}
+	
+	override render() {
+		const classes = {
+			selected: this.selected,
+			dark: this.theme === 'dark',
+		};
+		
+		return html`
 			<li class=${classMap(classes)}>
 				${this.img}
 				<strong>
@@ -79,47 +79,47 @@ export class CmdAction extends LitElement {
 				</strong>
 				${this.hotkeys}
 			</li>
-    `;
-  }
-  
-  /**
-   * Get hotkeys
-   * @private
-   */
-  private get hotkeys() {
-    if (this.action?.hotkey) {
-      const hotkeys = this.action.hotkey
-        .replace('cmd', '⌘')
-        .replace('shift', '⇧')
-        .replace('alt', '⌥')
-        .replace('ctrl', '⌃')
-        .toUpperCase()
-        .split('+');
-      return hotkeys.length > 0 ? html`<span>${repeat(hotkeys, (hotkey) => html`<kbd>${hotkey}</kbd>`,)}</span>` : '';
-    } else {
-      return nothing;
-    }
-  }
-  
-  /**
-   * Get description
-   * @private
-   */
-  private get description() {
-    return this.action.description ? html`<small>${this.action.description}</small>` : nothing;
-  }
-  
-  /**
-   * Get icon
-   * @private
-   */
-  private get img(): TemplateResult | typeof nothing {
-    return this.action.img ? html`<span>${unsafeHTML(this.action.img)}</span>` : nothing;
-  }
+		`;
+	}
+	
+	/**
+	 * Get hotkeys
+	 * @private
+	 */
+	private get hotkeys() {
+		if (this.action?.hotkey) {
+			const hotkeys = this.action.hotkey
+				.replace('cmd', '⌘')
+				.replace('shift', '⇧')
+				.replace('alt', '⌥')
+				.replace('ctrl', '⌃')
+				.toUpperCase()
+				.split('+');
+			return hotkeys.length > 0 ? html`<span>${repeat(hotkeys, (hotkey) => html`<kbd>${hotkey}</kbd>`,)}</span>` : '';
+		} else {
+			return nothing;
+		}
+	}
+	
+	/**
+	 * Get description
+	 * @private
+	 */
+	private get description() {
+		return this.action.description ? html`<small>${this.action.description}</small>` : nothing;
+	}
+	
+	/**
+	 * Get icon
+	 * @private
+	 */
+	private get img(): TemplateResult | typeof nothing {
+		return this.action.img ? html`<span>${unsafeHTML(this.action.img)}</span>` : nothing;
+	}
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'cmd-action': CmdAction;
-  }
+	interface HTMLElementTagNameMap {
+		'cmd-action': CmdAction;
+	}
 }
--- src/cmd-dialog.ts
+++ src/cmd-dialog.ts
@@ -13,184 +13,184 @@ import style from './style.css?inline';
 
 @customElement('cmd-dialog')
 export class CmdDialog extends LitElement {
-  
-  static override styles = unsafeCSS(style);
-  
-  /**
-   * The mode of the dialog (dark/light).
-   */
-  @property({type: String}) theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  
-  /**
-   * The placeholder text for the input.
-   */
-  @property({type: String}) placeholder = 'Type a command or search...';
-  
-  /**
-   * The footer notice for the dialog.
-   */
-  @property({type: String}) note = '';
-  
-  /**
-   * Open dialog hotkey
-   */
-  @property({type: String}) hotkey = 'cmd+k,ctrl+k';
-  
-  /**
-   * Array of actions
-   */
-  @property({
-    type: Array,
-    hasChanged() {
-      return true;
-    },
-  }) actions = [] as Array<Action>;
-  
-  /**
-   * Search input value
-   * @private
-   */
-  @state() private _search = '';
-  
-  /**
-   * Selected action
-   * @private
-   */
-  @state() private _selected?: Action;
-  
-  /**
-   * All actions that match the input
-   * @private
-   */
-  @state() private _results = [] as Array<Action>;
-  
-  /**
-   * Fuse.js instance
-   * @private
-   */
-  private fuse: Fuse<Action> | undefined;
-  
-  /**
-   * Open the dialog.
-   */
-  public open() {
-    this.dialog.showModal();
-  }
-  
-  /**
-   * Close the dialog.
-   */
-  public close() {
-    this.input.value = ''
-    this.dialog.close();
-  }
-  
-  override connectedCallback() {
-    super.connectedCallback();
-    
-    // open dialog
-    hotkeys(this.hotkey, (event) => {
-      this.open();
-      event.preventDefault();
-    });
-    
-    
-    // select next
-    hotkeys('down,tab', (event) => {
-      if (!this.dialog.open) return;
-      event.preventDefault();
-      
-      if (this._selectedIndex >= this._results.length - 1) {
-        this._selected = this._results[0];
-      } else {
-        this._selected = this._results[this._selectedIndex + 1];
-      }
-    });
-    
-    // select previous
-    hotkeys('up,shift+tab', (event) => {
-      if (!this.dialog.open) return;
-      event.preventDefault();
-      
-      if (this._selectedIndex === 0) {
-        this._selected = this._results[this._results.length - 1];
-      } else {
-        this._selected = this._results[this._selectedIndex - 1];
-      }
-    });
-    
-    // trigger action
-    hotkeys('enter', (event) => {
-      if (!this.dialog.open) return;
-      event.preventDefault();
-      this._triggerAction(this._results[this._selectedIndex]);
-    });
-  }
-  
-  override disconnectedCallback() {
-    super.disconnectedCallback();
-    // unregister hotkeys
-    hotkeys.unbind(this.hotkey);
-    hotkeys.unbind('down,tab');
-    hotkeys.unbind('up,shift+tab');
-    hotkeys.unbind('enter');
-  }
-  
-  override update(changedProperties: PropertyValues<this>) {
-    if (changedProperties.has('actions')) {
-      
-      // register action hotkeys
-      for (const action of this.actions.filter(item => !!item.hotkey)) {
-        hotkeys(action.hotkey || '', (event) => {
-          event.preventDefault();
-          this._triggerAction(action);
-        });
-      }
-      
-      // setup fuse search
-      this.fuse = new Fuse(this.actions,
-        {
-          //findAllMatches: true,
-          //includeMatches: true,
-          keys: [
-            {name: 'title', weight: 2},
-            {name: 'tags', weight: 1},
-            {name: 'url', weight: 1}
-          ],
-        });
-    }
-    
-    super.update(changedProperties);
-  }
-  
-  override render() {
-    // search for matches
-    const results = this.fuse?.search(this._search);
-    if (results) {
-      this._results = results.map(item => item.item);
-    }
-    
-    if (this._search.length > 0) {
-      const results = this.fuse?.search(this._search);
-      if (results) {
-        this._results = results.map(item => item.item);
-      }
-    } else {
-      this._results = this.actions;
-    }
-    
-    // select first result
-    if (this._results.length > 0 && this._selectedIndex === -1) {
-      this._selected = this._results[0];
-    }
-    
-    // nothing was found
-    if (this._results.length === 0) {
-      this._selected = undefined;
-    }
-    
-    const actionList: TemplateResult =
-      html`
+	
+	static override styles = unsafeCSS(style);
+	
+	/**
+	 * The mode of the dialog (dark/light).
+	 */
+	@property({type: String}) theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+	
+	/**
+	 * The placeholder text for the input.
+	 */
+	@property({type: String}) placeholder = 'Type a command or search...';
+	
+	/**
+	 * The footer notice for the dialog.
+	 */
+	@property({type: String}) note = '';
+	
+	/**
+	 * Open dialog hotkey
+	 */
+	@property({type: String}) hotkey = 'cmd+k,ctrl+k';
+	
+	/**
+	 * Array of actions
+	 */
+	@property({
+		type: Array,
+		hasChanged() {
+			return true;
+		},
+	}) actions = [] as Array<Action>;
+	
+	/**
+	 * Search input value
+	 * @private
+	 */
+	@state() private _search = '';
+	
+	/**
+	 * Selected action
+	 * @private
+	 */
+	@state() private _selected?: Action;
+	
+	/**
+	 * All actions that match the input
+	 * @private
+	 */
+	@state() private _results = [] as Array<Action>;
+	
+	/**
+	 * Fuse.js instance
+	 * @private
+	 */
+	private fuse: Fuse<Action> | undefined;
+	
+	/**
+	 * Open the dialog.
+	 */
+	public open() {
+		this.dialog.showModal();
+	}
+	
+	/**
+	 * Close the dialog.
+	 */
+	public close() {
+		this.input.value = ''
+		this.dialog.close();
+	}
+	
+	override connectedCallback() {
+		super.connectedCallback();
+		
+		// open dialog
+		hotkeys(this.hotkey, (event) => {
+			this.open();
+			event.preventDefault();
+		});
+		
+		
+		// select next
+		hotkeys('down,tab', (event) => {
+			if (!this.dialog.open) return;
+			event.preventDefault();
+			
+			if (this._selectedIndex >= this._results.length - 1) {
+				this._selected = this._results[0];
+			} else {
+				this._selected = this._results[this._selectedIndex + 1];
+			}
+		});
+		
+		// select previous
+		hotkeys('up,shift+tab', (event) => {
+			if (!this.dialog.open) return;
+			event.preventDefault();
+			
+			if (this._selectedIndex === 0) {
+				this._selected = this._results[this._results.length - 1];
+			} else {
+				this._selected = this._results[this._selectedIndex - 1];
+			}
+		});
+		
+		// trigger action
+		hotkeys('enter', (event) => {
+			if (!this.dialog.open) return;
+			event.preventDefault();
+			this._triggerAction(this._results[this._selectedIndex]);
+		});
+	}
+	
+	override disconnectedCallback() {
+		super.disconnectedCallback();
+		// unregister hotkeys
+		hotkeys.unbind(this.hotkey);
+		hotkeys.unbind('down,tab');
+		hotkeys.unbind('up,shift+tab');
+		hotkeys.unbind('enter');
+	}
+	
+	override update(changedProperties: PropertyValues<this>) {
+		if (changedProperties.has('actions')) {
+			
+			// register action hotkeys
+			for (const action of this.actions.filter(item => !!item.hotkey)) {
+				hotkeys(action.hotkey || '', (event) => {
+					event.preventDefault();
+					this._triggerAction(action);
+				});
+			}
+			
+			// setup fuse search
+			this.fuse = new Fuse(this.actions,
+				{
+					//findAllMatches: true,
+					//includeMatches: true,
+					keys: [
+						{name: 'title', weight: 2},
+						{name: 'tags', weight: 1},
+						{name: 'url', weight: 1}
+					],
+				});
+		}
+		
+		super.update(changedProperties);
+	}
+	
+	override render() {
+		// search for matches
+		const results = this.fuse?.search(this._search);
+		if (results) {
+			this._results = results.map(item => item.item);
+		}
+		
+		if (this._search.length > 0) {
+			const results = this.fuse?.search(this._search);
+			if (results) {
+				this._results = results.map(item => item.item);
+			}
+		} else {
+			this._results = this.actions;
+		}
+		
+		// select first result
+		if (this._results.length > 0 && this._selectedIndex === -1) {
+			this._selected = this._results[0];
+		}
+		
+		// nothing was found
+		if (this._results.length === 0) {
+			this._selected = undefined;
+		}
+		
+		const actionList: TemplateResult =
+			html`
 				<ul>
 					${repeat(
 						this._results,
@@ -207,8 +207,8 @@ export class CmdDialog extends LitElement {
 								></cmd-action>`
 					)}
 				</ul>`;
-    
-    return html`
+		
+		return html`
 			<dialog
 				class="${this.theme}"
 				@close="${this.close}"
@@ -237,97 +237,97 @@ export class CmdDialog extends LitElement {
 					${unsafeHTML(this.note || `<span>${this._results.length} options</span>`)}
 				</slot>
 			</dialog>
-    `;
-  }
-  
-  /**
-   * Render the results on input.
-   * @param event
-   * @private
-   */
-  private async _onInput(event: InputEvent) {
-    const input = event.target as HTMLInputElement;
-    this._search = input.value;
-    await this.updateComplete;
-    
-    this.dispatchEvent(
-      new CustomEvent(
-        'change', {
-          detail: {
-            search: input.value,
-          },
-          bubbles: true,
-          composed: true,
-        })
-    )
-  }
-  
-  /**
-   * Handle focus on action.
-   * @param action
-   * @private
-   */
-  private _actionFocused(action: Action) {
-    this._selected = action;
-  }
-  
-  /**
-   * Trigger the action.
-   * @param action
-   * @private
-   */
-  private _triggerAction(action?: Action) {
-    this._selected = action;
-    
-    // fire selected event even when action is empty/not selected,
-    // so possible handle api search for example
-    this.dispatchEvent(
-      new CustomEvent('selected', {
-        detail: {search: this._search, action},
-        bubbles: true,
-        composed: true,
-      })
-    );
-    
-    if (!action) return;
-    
-    // trigger action
-    if (action.handler) {
-      const result = action.handler(action);
-      if (!result?.keepOpen) {
-        this.close();
-      }
-    } else if (action.url) {
-      window.open(action.url, action.target || '_self');
-      this.close();
-    }
-  }
-  
-  /**
-   * Return the index of the selected action.
-   * @private
-   */
-  private get _selectedIndex(): number {
-    return !this._selected ? -1 : this._results.indexOf(this._selected);
-  }
-  
-  /**
-   * Return the dialog element.
-   */
-  get dialog(): HTMLDialogElement {
-    return this.shadowRoot?.querySelector('dialog') as HTMLDialogElement;
-  }
-  
-  /**
-   * Return the input element.
-   */
-  get input(): HTMLInputElement {
-    return this.shadowRoot?.querySelector('input') as HTMLInputElement;
-  }
+		`;
+	}
+	
+	/**
+	 * Render the results on input.
+	 * @param event
+	 * @private
+	 */
+	private async _onInput(event: InputEvent) {
+		const input = event.target as HTMLInputElement;
+		this._search = input.value;
+		await this.updateComplete;
+		
+		this.dispatchEvent(
+			new CustomEvent(
+				'change', {
+					detail: {
+						search: input.value,
+					},
+					bubbles: true,
+					composed: true,
+				})
+		)
+	}
+	
+	/**
+	 * Handle focus on action.
+	 * @param action
+	 * @private
+	 */
+	private _actionFocused(action: Action) {
+		this._selected = action;
+	}
+	
+	/**
+	 * Trigger the action.
+	 * @param action
+	 * @private
+	 */
+	private _triggerAction(action?: Action) {
+		this._selected = action;
+		
+		// fire selected event even when action is empty/not selected,
+		// so possible handle api search for example
+		this.dispatchEvent(
+			new CustomEvent('selected', {
+				detail: {search: this._search, action},
+				bubbles: true,
+				composed: true,
+			})
+		);
+		
+		if (!action) return;
+		
+		// trigger action
+		if (action.handler) {
+			const result = action.handler(action);
+			if (!result?.keepOpen) {
+				this.close();
+			}
+		} else if (action.url) {
+			window.open(action.url, action.target || '_self');
+			this.close();
+		}
+	}
+	
+	/**
+	 * Return the index of the selected action.
+	 * @private
+	 */
+	private get _selectedIndex(): number {
+		return !this._selected ? -1 : this._results.indexOf(this._selected);
+	}
+	
+	/**
+	 * Return the dialog element.
+	 */
+	get dialog(): HTMLDialogElement {
+		return this.shadowRoot?.querySelector('dialog') as HTMLDialogElement;
+	}
+	
+	/**
+	 * Return the input element.
+	 */
+	get input(): HTMLInputElement {
+		return this.shadowRoot?.querySelector('input') as HTMLInputElement;
+	}
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'cmd-dialog': CmdDialog;
-  }
+	interface HTMLElementTagNameMap {
+		'cmd-dialog': CmdDialog;
+	}
 }
