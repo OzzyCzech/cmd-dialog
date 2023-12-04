352661775089f7bba00b5fd4cc0da52db4ec6f09
Roman Ožana <roman@ozana.cz>
2023-12-04 10:42:19 +0100

    Private firs

--- src/cmd-action.ts
+++ src/cmd-action.ts
@@ -3,7 +3,6 @@ import {customElement, property} from 'lit/decorators.js';
 import {classMap} from 'lit/directives/class-map.js';
 import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 import {repeat} from 'lit/directives/repeat.js';
-
 import {type Action} from './action.js';
 import style from './style.css?inline'; // eslint-disable-line n/file-extension-in-import
 
@@ -32,6 +31,40 @@ export class CmdAction extends LitElement {
 	constructor() {
 		super();
 		this.addEventListener('click', this.click);
+	}
+
+	/**
+	 * Get hotkeys
+	 * @private
+	 */
+	private get hotkeys() {
+		if (this.action?.hotkey) {
+			const hotkeys = this.action.hotkey
+				.replace('cmd', '⌘')
+				.replace('shift', '⇧')
+				.replace('alt', '⌥')
+				.replace('ctrl', '⌃')
+				.split('+');
+			return hotkeys.length > 0 ? html`<span>${repeat(hotkeys, hotkey => html`<kbd part="kbd">${hotkey}</kbd>`)}</span>` : '';
+		}
+
+		return nothing;
+	}
+
+	/**
+	 * Get description
+	 * @private
+	 */
+	private get description() {
+		return this.action.description ? html`<small part="description">${this.action.description}</small>` : nothing;
+	}
+
+	/**
+	 * Get icon
+	 * @private
+	 */
+	private get img(): TemplateResult | typeof nothing {
+		return this.action.img ? html`<span>${unsafeHTML(this.action.img)}</span>` : nothing;
 	}
 
 	/**
@@ -82,40 +115,6 @@ export class CmdAction extends LitElement {
 				${this.hotkeys}
 			</li>
 		`;
-	}
-
-	/**
-	 * Get hotkeys
-	 * @private
-	 */
-	private get hotkeys() {
-		if (this.action?.hotkey) {
-			const hotkeys = this.action.hotkey
-				.replace('cmd', '⌘')
-				.replace('shift', '⇧')
-				.replace('alt', '⌥')
-				.replace('ctrl', '⌃')
-				.split('+');
-			return hotkeys.length > 0 ? html`<span>${repeat(hotkeys, hotkey => html`<kbd part="kbd">${hotkey}</kbd>`)}</span>` : '';
-		}
-
-		return nothing;
-	}
-
-	/**
-	 * Get description
-	 * @private
-	 */
-	private get description() {
-		return this.action.description ? html`<small part="description">${this.action.description}</small>` : nothing;
-	}
-
-	/**
-	 * Get icon
-	 * @private
-	 */
-	private get img(): TemplateResult | typeof nothing {
-		return this.action.img ? html`<span>${unsafeHTML(this.action.img)}</span>` : nothing;
 	}
 }
 
