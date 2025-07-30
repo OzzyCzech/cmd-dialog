c7f4079ab9d17c8646fc03119bfc0c2634d30728
Roman Ožana <roman@ozana.cz>
2025-07-30 07:33:41 +0200

    Ignore types (they are generated)

--- types/cmd-action.d.ts
+++ types/cmd-action.d.ts
@@ -1,55 +1,55 @@
 import { LitElement, type TemplateResult } from "lit";
 import type { Action } from "./action.js";
 export declare class CmdAction extends LitElement {
-	/**
-	 * The styles
-	 */
-	static styles: import("lit").CSSResult;
-	/**
-	 * The mode of the dialog (dark/light).
-	 */
-	theme: string;
-	/**
-	 * Action object
-	 */
-	action: Action;
-	/**
-	 * Is the action selected
-	 */
-	selected: boolean;
-	constructor();
-	/**
-	 * Get hotkeys
-	 * @private
-	 */
-	private get hotkeys();
-	/**
-	 * Get description
-	 * @private
-	 */
-	private get description();
-	/**
-	 * Get icon
-	 * @private
-	 */
-	private get img();
-	/**
-	 * Scroll to show element
-	 */
-	ensureInView(): void;
-	/**
-	 * Click event
-	 */
-	click(): void;
-	/**
-	 * Updated
-	 * @param changedProperties
-	 */
-	updated(changedProperties: Map<string, unknown>): void;
-	render(): TemplateResult<1>;
+    /**
+     * The styles
+     */
+    static styles: import("lit").CSSResult;
+    /**
+     * The mode of the dialog (dark/light).
+     */
+    theme: string;
+    /**
+     * Action object
+     */
+    action: Action;
+    /**
+     * Is the action selected
+     */
+    selected: boolean;
+    constructor();
+    /**
+     * Get hotkeys
+     * @private
+     */
+    private get hotkeys();
+    /**
+     * Get description
+     * @private
+     */
+    private get description();
+    /**
+     * Get icon
+     * @private
+     */
+    private get img();
+    /**
+     * Scroll to show element
+     */
+    ensureInView(): void;
+    /**
+     * Click event
+     */
+    click(): void;
+    /**
+     * Updated
+     * @param changedProperties
+     */
+    updated(changedProperties: Map<string, unknown>): void;
+    render(): TemplateResult<1>;
 }
 declare global {
-	interface HTMLElementTagNameMap {
-		"cmd-action": CmdAction;
-	}
+    interface HTMLElementTagNameMap {
+        "cmd-action": CmdAction;
+    }
 }
